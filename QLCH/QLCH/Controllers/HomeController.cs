f9dbe1e707a3ded19b264908db45af150ffd3a4b
huudangphan <64909707+huudangphan@users.noreply.github.com>
2021-11-03 12:29:19 +0700

    Kết nối với entities framework

--- QLCH/QLCH/Controllers/HomeController.cs
+++ QLCH/QLCH/Controllers/HomeController.cs
@@ -3,17 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using QLCH.Models;
+
 namespace QLCH.Controllers
 {
     public class HomeController : Controller
     {
-        QLDienMayEntities db = new QLDienMayEntities();
-        public ActionResult test()
-        {
-            var result = db.test();
-            return View(result);
-        }
+        
         public ActionResult Index()
         {
             return View();
